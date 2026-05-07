76e5ecf3e06ee929498df80240a60fcdca95e6d7
sindakadoussi <sindakadoussi@gmail.com>
2026-05-07 10:19:03 +0200

    routes

---
+++ frontend/src/api/entretiens.js
@@ -0,0 +1,3 @@
+export const entretiensRoutes = {
+  list: '/api/entretiens',
+};
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,16 +1,9 @@
-// HTTP Client
-export { apiClient } from './http-client.js';
-
-// Constants
-export { API_ROUTES, WEBHOOK_ROUTES, API_BASE, WEBHOOK } from './constants.js';
-
-// Services
-export { authService } from './auth-service.js';
-export { boursesService } from './bourses-service.js';
-export { messagesService } from './messages-service.js';
-export { roadmapService } from './roadmap-service.js';
-export { entretiensService } from './entretiens-service.js';
-export { usersService } from './users-service.js';
-export { favorisService } from './favoris-service.js';
-export { feedbacksService } from './feedbacks-service.js';
-export { scoreMatchService } from './score-match-service.js';
+export * from './auth';
+export * from './bourses';
+export * from './roadmap';
+export * from './messages';
+export * from './entretiens';
+export * from './users';
+export * from './favoris';
+export * from './feedbacks';
+export * from './webhook';
